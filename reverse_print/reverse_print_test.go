d7e468409b4ca96c3b0183dab0d4b4a8fa481090
yongquan <605825096@qq.com>
2020-09-01 00:10:16 +0800

    增加反转输入

--- reverse_print/reverse_print_test.go
+++ reverse_print/reverse_print_test.go
@@ -17,3 +17,22 @@ func TestReversePrint(t *testing.T) {
 	}
 	fmt.Println(reversePrint(&head))
 }
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
+func TestGo(t *testing.T) {
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+
+	fmt.Println(<- c)
+	fmt.Println(<- c)
+}
