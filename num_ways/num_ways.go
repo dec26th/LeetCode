d770d8ebb926c98d599f6bb46c21553da3c8bd58
Yongquan <alpha605825096@gmail.com>
2020-09-01 01:31:32 +0800

    stupid way to do dp

---
+++ num_ways/num_ways.go
@@ -0,0 +1,18 @@
+package num_ways
+
+
+func numWays(n int) int {
+	if n == 0 {
+		return 1
+	}
+
+	if n == 1 || n == 2 {
+		return n
+	}
+	way := make([]int, n + 1)
+	way[1], way[2] = 1, 2
+	for i := 3; i < n + 1; i ++ {
+		way[i] = (way[i - 1] + way[i - 2])% (1e9 + 7)
+	}
+	return way[n]
+}
