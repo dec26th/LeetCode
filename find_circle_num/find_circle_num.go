a75e7b4d0907da6621ab9f94ca4ef8dc0317799a
Yongquan <alpha605825096@gmail.com>
2020-09-11 01:00:11 +0800

    更新todo

--- find_circle_num/find_circle_num.go
+++ find_circle_num/find_circle_num.go
@@ -2,6 +2,7 @@ package find_circle_num
 
 var line, coluomn int
 
+//todo 和岛屿数量一样
 func findCircleNum(M [][]int) int {
 	return 0
 }
--- trap/trap.go
+++ trap/trap.go
@@ -1,5 +1,7 @@
 package trap
 
+
+//todo 接雨水
 func trap(height []int) int {
 	return 0
 }
