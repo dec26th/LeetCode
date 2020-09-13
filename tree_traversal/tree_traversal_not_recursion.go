91aa20da3bc7117ac8cde4a46a207bf523f056fd
Yongquan <alpha605825096@gmail.com>
2020-09-13 22:34:28 +0800

    update

--- tree_traversal/tree_traversal_not_recursion.go
+++ tree_traversal/tree_traversal_not_recursion.go
@@ -1,1 +1,42 @@
 package tree_traversal
+
+func preorderTraversalNotResursion(root *TreeNode) []int {
+	stack := *new([] *TreeNode)
+	result := *new([] int)
+
+	for 0 < len(stack) || root != nil {
+		for root != nil {
+			result = append(result, root.Val)
+			stack = append(stack, root.Right)
+			root = root.Left
+		}
+
+		index := len(stack) - 1
+		root = stack[index]
+		stack = stack[:index]
+	}
+
+	return result
+}
+
+func inorderTraversalNotResursion(root *TreeNode) []int {
+	stack := *new([] *TreeNode)
+	result := *new([] int)
+
+	for 0 < len(stack) || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+
+		index := len(stack) - 1
+		root = stack[index]
+		stack = stack[:index]
+
+		result = append(result, root.Val)
+		root = root.Right
+
+	}
+
+	return result
+}
