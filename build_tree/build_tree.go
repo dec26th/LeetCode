9e695459919c6e58cc24d9f4657454ae1fbb608e
Yongquan <alpha605825096@gmail.com>
2020-10-10 00:14:59 +0800

    增加前序中序遍历得到树结构，增加两个栈实现队列

---
+++ build_tree/build_tree.go
@@ -0,0 +1,29 @@
+package build_tree
+
+// 剑指Offer 07 重建二叉树
+// 输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+
+type TreeNode struct {
+	Val		int
+	Left	*TreeNode
+	Right	*TreeNode
+}
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+
+	head := new(TreeNode)
+	head.Val = preorder[0]
+	i := 0
+	for ; i < len(inorder); i++ {
+		if inorder[i] == head.Val {
+			break
+		}
+	}
+
+	head.Left = buildTree(preorder[1 : len(inorder[:i]) + 1], inorder[:i])
+	head.Right = buildTree(preorder[len(inorder[:i]) + 1:], inorder[i + 1:])
+	return head
+}
---
+++ c_queue/c_queue.go
@@ -0,0 +1,24 @@
+package c_queue
+
+// 剑指 Offer 09 用两个栈实现队列
+// 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+// 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+
+type CQueue struct {
+
+}
+
+
+func Constructor() CQueue {
+
+}
+
+
+func (this *CQueue) AppendTail(value int)  {
+
+}
+
+
+func (this *CQueue) DeleteHead() int {
+
+}
