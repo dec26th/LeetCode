b7a16b4654f9ceeada74a1d9e30db3185bd2cace
黄泳全 <huangyongquan.1226@bytedance.com>
2020-10-20 15:06:03 +0800

    关于select 的测试

--- main.go
+++ main.go
@@ -1,8 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
 
 func main() {
-	test := make([]bool, 1)
-	fmt.Println(test[0])
+	runtime.GOMAXPROCS(3)
+	chan1 := make(chan int, 10)
+	chan2 := make(chan int, 10)
+
+	wg.Add(3)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 100; i++ {
+			chan1 <- i
+			fmt.Println("chan1")
+		}
+	}()
+
+	go func () {
+		defer wg.Done()
+		for i := 100; i < 200; i++ {
+			chan2 <- i
+			fmt.Println("chan2")
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case c := <-chan1:
+				fmt.Printf("select from chan1 %d \n", c)
+			case c := <-chan2:
+				fmt.Printf("select from chan2 %d \n", c)
+
+			}
+		}
+	}()
+
+	wg.Wait()
 }
