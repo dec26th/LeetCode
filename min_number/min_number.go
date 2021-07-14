be827f3ff61b319e5fb579446f98c731847bdde1
yongquan <605825096@qq.com>
2021-07-14 23:07:15 +0800

    updateeeeeeeeee

--- min_number/min_number.go
+++ min_number/min_number.go
@@ -1,11 +1,49 @@
 package min_number
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 func minNumber(nums []int) string {
-	result := make([]string, len(nums))
-	
-
+	result := getMinNumberStringList(nums, 1)
 
 	return strings.Join(result, "")
+}
+
+func getMinNumberStringList(nums []int, index int) []string {
+	tempMap := make(map[int][]int, 10)
+	for i := 0; i < len(nums); i++ {
+		x := getFirstNum(nums[i], index)
+		if _, ok := tempMap[x]; !ok {
+			tempMap[x] = []int {
+				nums[i],
+			}
+		} else {
+			tempMap[x] = append(tempMap[x], nums[i])
+		}
+	}
+
+	result := make([]string, len(nums))
+	for i := 0; i < 10; i++ {
+		if _, ok := tempMap[i]; !ok {
+			continue
+		} else {
+			if len(tempMap[i]) == 1 {
+				result = append(result, strconv.Itoa(tempMap[i][0]))
+			} else {
+				result = append(result, getMinNumberStringList(tempMap[i],index + 1)...)
+			}
+		}
+	}
+	return result
+}
+
+func getFirstNum(num int, index int) int {
+	nums := strconv.Itoa(num)
+	if len(nums) - 1 < index {
+		return int(nums[len(nums) - 1] - '0')
+	} else {
+		return int(nums[index - 1] - '0')
+	}
 }
---
+++ min_number/min_number_test.go
@@ -0,0 +1,18 @@
+package min_number
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMinNumber(t *testing.T) {
+	temp := []int {
+		20,
+		1,
+	}
+	fmt.Println(minNumber(temp))
+}
+
+func TestLess(t *testing.T) {
+	fmt.Println("100000000" < "3")
+}
