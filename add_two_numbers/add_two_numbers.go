c15a974d7933e758dec608f29d0b9dbe4ffa6049
huangyongquan.dec26 <huangyongquan.dec26@bytedance.com>
2021-12-06 20:56:29 +0800

    update
    Change-Id: Ibd7fcc61e534953bc844936cb0e9926a84375a79

--- add_two_numbers/add_two_numbers.go
+++ add_two_numbers/add_two_numbers.go
@@ -92,3 +92,30 @@ func addTwoNumbersTwo(l1 *ListNode, l2 *ListNode) *ListNode {
 
 	return head.Next
 }
+
+func addTwoNumbersThree(l1 *ListNode, l2 *ListNode) *ListNode {
+	head := new(ListNode)
+
+	cur := head
+	carry := 0
+
+	for l1 != nil || l2 != nil || carry != 0 {
+		cur.Next = new(ListNode)
+		cur = cur.Next
+
+		if l1 != nil {
+			carry += l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			carry += l2.Val
+			l2 = l2.Next
+		}
+
+		cur.Val = carry % 10
+		carry = carry / 10
+	}
+
+	return head.Next
+}
